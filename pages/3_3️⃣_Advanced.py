3ed6a8eb950e908764be4bdcd8a4870c14000191
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 13:30:38 +0700

    Update 3_3️⃣_Advanced.py

--- pages/3_3️⃣_Advanced.py
+++ pages/3_3️⃣_Advanced.py
@@ -37,7 +37,8 @@ st.dataframe(input_df, hide_index=True)
 with st.sidebar:
     st.header('⚙️ Model Settings')
     parameter_split_size = st.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
-
+    parameter_n_estimators = st.slider('Number of estimators (n_estimators)', 0, 1000, 100, 100)
+    parameter_max_features = st.slider('Max features (max_features)', 0, 5, 1)
 
 # Data pre-processing
 
