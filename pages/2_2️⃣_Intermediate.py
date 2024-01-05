9649e8728e975b593e9c355c4256ae01e9c02e72
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 12:34:10 +0700

    Update 2_2️⃣_Intermediate.py

--- pages/2_2️⃣_Intermediate.py
+++ pages/2_2️⃣_Intermediate.py
@@ -8,3 +8,31 @@ st.set_page_config(
 
 st.title('🐧 Palmer Penguins Predictor')
 st.warning('Intermediate Mode')
+
+st.subheader('Prediction Probability')
+st.dataframe(prediction_proba, 
+             column_config={
+                '0': st.column_config.ProgressColumn(
+                    'Adelie',
+                    format='%f',
+                    width='medium',
+                    min_value=0,
+                    max_value=1,
+                ),
+                '1': st.column_config.ProgressColumn(
+                    'Chinstrap',
+                    format='%f',
+                    width='medium',
+                    min_value=0,
+                    max_value=1,
+                ),
+                '2': st.column_config.ProgressColumn(
+                    'Gentoo',
+                    format='%f',
+                    width='medium',
+                    min_value=0,
+                    max_value=1,
+                ),
+             },
+             hide_index=True,
+          )
