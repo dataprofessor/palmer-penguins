154cd55c5738e33596027c364314ed7a1210890a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 09:59:51 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -7,37 +7,4 @@ st.set_page_config(
 
 st.title("🐧 Palmer Penguins Predictor")
 
-# User input features
-def user_input_features():
-    island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-    sex = st.sidebar.selectbox('Sex',('male','female'))
-    bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-    bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-    flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-    body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-    data = {'island': island,
-            'bill_length_mm': bill_length_mm,
-            'bill_depth_mm': bill_depth_mm,
-            'flipper_length_mm': flipper_length_mm,
-            'body_mass_g': body_mass_g,
-            'sex': sex}
-    features = pd.DataFrame(data, index=[0])
-    return features
-input_df = user_input_features()
-
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'], axis=1)
-df = pd.concat([input_df,penguins],axis=0)
-
-# Encoding ordinal features
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
-df = df[:1] # Selects only the first row (the user input data)
-
-st.write(df)
 
