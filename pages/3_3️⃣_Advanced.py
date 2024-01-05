efc9900fce082d0875d790738c2e6a836cfe3d19
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 15:14:28 +0700

    Update 3_3️⃣_Advanced.py

--- pages/3_3️⃣_Advanced.py
+++ pages/3_3️⃣_Advanced.py
@@ -143,8 +143,6 @@ st.write('Confusion matrix:')
 predictions = clf.predict(X)
 cm = confusion_matrix(y, predictions, labels=clf.classes_)
 
-st.write(cm)
-
 
 labels = np.unique(y)
 cm = [y for i in cm for y in i]
@@ -171,5 +169,3 @@ cm_plot = alt.Chart(df_cm).mark_rect().encode(
                 alt.selection_interval(encodings=['x'], empty='none')
             )
 st.altair_chart(cm_plot)
-
-df_cm
