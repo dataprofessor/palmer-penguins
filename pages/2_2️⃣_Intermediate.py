673f5f9b5c6ba2de6d8746dee9abd433ddd19726
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 12:39:30 +0700

    Update 2_2️⃣_Intermediate.py

--- pages/2_2️⃣_Intermediate.py
+++ pages/2_2️⃣_Intermediate.py
@@ -29,8 +29,6 @@ data = {'island': island,
         'gender': gender}
 input_df = pd.DataFrame(data, index=[0])
 
-st.dataframe(input_df)
-
 # Data pre-processing
 
 ## Combines user input features with entire penguins dataset
@@ -44,6 +42,8 @@ encode = ['island','gender']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 
 input_row = df_penguins[:1] # Selects only the first row (the user input data)
+
+st.dataframe(input_row)
 
 ## Preparing the dataframe
 target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
