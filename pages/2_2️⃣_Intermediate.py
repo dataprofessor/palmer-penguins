74b21d7ea610a2d1d52f2721fa8b5b68a7e22302
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 12:34:52 +0700

    Update 2_2️⃣_Intermediate.py

--- pages/2_2️⃣_Intermediate.py
+++ pages/2_2️⃣_Intermediate.py
@@ -9,6 +9,68 @@ st.set_page_config(
 st.title('🐧 Palmer Penguins Predictor')
 st.warning('Intermediate Mode')
 
+# User input features
+st.subheader('Input features')
+with st.container(border=True):
+    island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+    gender = st.selectbox('Gender',('male','female'))
+    bill_length_mm = st.slider('Bill length (mm)', 32.1,59.6,43.9)
+    bill_depth_mm = st.slider('Bill depth (mm)', 13.1,21.5,17.2)
+    flipper_length_mm = st.slider('Flipper length (mm)', 172.0,231.0,201.0)
+    body_mass_g = st.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+
+data = {'island': island,
+        'bill_length_mm': bill_length_mm,
+        'bill_depth_mm': bill_depth_mm,
+        'flipper_length_mm': flipper_length_mm,
+        'body_mass_g': body_mass_g,
+        'gender': gender}
+input_df = pd.DataFrame(data, index=[0])
+
+# Data pre-processing
+
+## Combines user input features with entire penguins dataset
+## This will be useful for the encoding phase
+penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/master/data/penguins_cleaned.csv')
+penguins = penguins_raw.drop('species', axis=1)
+input_penguins = pd.concat([input_df, penguins],axis=0)
+
+## Encoding ordinal features
+encode = ['island','gender']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+input_row = df_penguins[:1] # Selects only the first row (the user input data)
+
+st.write(input_row)
+
+## Preparing the dataframe
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+def target_encode(val):
+    return target_mapper[val]
+
+#df = penguins_raw.copy()
+
+#for col in encode:
+#    dummy = pd.get_dummies(df[col], prefix=col)
+#    df = pd.concat([df,dummy], axis=1)
+#    del df[col]
+
+## Separating X and y
+X = df_penguins[1:]
+y = penguins_raw['species'].apply(target_encode)
+
+# Train ML model
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+# Apply model to make predictions
+prediction = clf.predict(input_row)
+prediction_proba = clf.predict_proba(input_row)
+
+st.subheader('Predicted Species')
+penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+st.success(str(penguins_species[prediction][0]))
+
 st.subheader('Prediction Probability')
 st.dataframe(prediction_proba, 
              column_config={
