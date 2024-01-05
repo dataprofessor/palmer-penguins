df2d33d1adb9c1e455b819f4244d1eed9c19d605
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 12:36:01 +0700

    Update 2_2️⃣_Intermediate.py

--- pages/2_2️⃣_Intermediate.py
+++ pages/2_2️⃣_Intermediate.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config(
     page_title='Palmer Penguins Predictor - Intermediate Mode',
