20fface2c563ca136a5a1e20156caf91a4f3ff40
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 10:08:32 +0700

    Update 3_3️⃣_Advanced.py

--- pages/3_3️⃣_Advanced.py
+++ pages/3_3️⃣_Advanced.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Advanced", page_icon="3️⃣")
+st.set_page_config(page_title='Advanced', page_icon='3️⃣')
 
-st.title("3️⃣ Advanced")
+st.title('3️⃣ Advanced')
