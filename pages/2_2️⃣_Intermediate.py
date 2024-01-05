e47a8242cb12d1278d9679590a2feab53b026140
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 10:08:17 +0700

    Update 2_2️⃣_Intermediate.py

--- pages/2_2️⃣_Intermediate.py
+++ pages/2_2️⃣_Intermediate.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Intermediate", page_icon="2️⃣")
+st.set_page_config(page_title='Intermediate', page_icon='2️⃣')
 
-st.title("2️⃣ Intermediate")
+st.title('2️⃣ Intermediate')
 
