ddc9413ca5d992f5ba02b7a0eadd712e47d429ab
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 12:33:51 +0700

    Update 1_1️⃣_Easy.py

--- pages/1_1️⃣_Easy.py
+++ pages/1_1️⃣_Easy.py
@@ -73,30 +73,3 @@ st.subheader('Predicted Species')
 penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
 st.success(str(penguins_species[prediction][0]))
 
-st.subheader('Prediction Probability')
-st.dataframe(prediction_proba, 
-             column_config={
-                '0': st.column_config.ProgressColumn(
-                    'Adelie',
-                    format='%f',
-                    width='medium',
-                    min_value=0,
-                    max_value=1,
-                ),
-                '1': st.column_config.ProgressColumn(
-                    'Chinstrap',
-                    format='%f',
-                    width='medium',
-                    min_value=0,
-                    max_value=1,
-                ),
-                '2': st.column_config.ProgressColumn(
-                    'Gentoo',
-                    format='%f',
-                    width='medium',
-                    min_value=0,
-                    max_value=1,
-                ),
-             },
-             hide_index=True,
-          )
