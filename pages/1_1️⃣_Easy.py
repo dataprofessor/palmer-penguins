9f8a209d82d0bf45951fee6841d6aa7a1723a887
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 10:06:33 +0700

    Update 1_1️⃣_Easy.py

--- pages/1_1️⃣_Easy.py
+++ pages/1_1️⃣_Easy.py
@@ -9,22 +9,21 @@ st.set_page_config(
 st.title("🐧 Palmer Penguins Predictor - Easy Mode")
 
 # User input features
-def user_input_features():
-    island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-    gender = st.sidebar.selectbox('Gender',('male','female'))
-    bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-    bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-    flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-    body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-    data = {'island': island,
-            'bill_length_mm': bill_length_mm,
-            'bill_depth_mm': bill_depth_mm,
-            'flipper_length_mm': flipper_length_mm,
-            'body_mass_g': body_mass_g,
-            'gender': gender}
-    features = pd.DataFrame(data, index=[0])
-    return features
-input_df = user_input_features()
+with st.container(border=True):
+    island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+    gender = st.selectbox('Gender',('male','female'))
+    bill_length_mm = st.slider('Bill length (mm)', 32.1,59.6,43.9)
+    bill_depth_mm = st.slider('Bill depth (mm)', 13.1,21.5,17.2)
+    flipper_length_mm = st.slider('Flipper length (mm)', 172.0,231.0,201.0)
+    body_mass_g = st.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+
+data = {'island': island,
+        'bill_length_mm': bill_length_mm,
+        'bill_depth_mm': bill_depth_mm,
+        'flipper_length_mm': flipper_length_mm,
+        'body_mass_g': body_mass_g,
+        'gender': gender}
+input_df = pd.DataFrame(data, index=[0])
 
 # Combines user input features with entire penguins dataset
 # This will be useful for the encoding phase
