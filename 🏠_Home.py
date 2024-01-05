2cc2f0659abea334c029cac1b34e086a3bdc4052
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-05 10:09:04 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Palmer Penguins Predictor",
-    page_icon="🐧",
+    page_title='Palmer Penguins Predictor',
+    page_icon='🐧',
 )
 
-st.title("🐧 Palmer Penguins Predictor")
-
+st.title('🐧 Palmer Penguins Predictor')
+st.write('Built in 3 levels of difficulty!')
 
