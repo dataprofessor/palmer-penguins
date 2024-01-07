a5f3d7d45cc77ba9fb4be04867d334e469378932
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-07 10:06:01 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -9,3 +9,16 @@ st.set_page_config(
 st.title('🐧 Palmer Penguins Predictor')
 st.info('Built in 3 levels of difficulty!')
 
+col = st.columns(3)
+with col[0]:
+    btn_easy = st.button('Easy')
+    if btn_easy:
+        switch_page('Easy')
+with col[1]:
+    btn_intermediate = st.button('Intermediate')
+    if btn_intermediate:
+        switch_page('Intermediate')
+with col[2]:
+    btn_advanced = st.button('Advanced')
+    if btn_advanced:
+        switch_page('Advanced')
