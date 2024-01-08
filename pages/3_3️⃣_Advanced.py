d2d93270dc33d99763fe4ea0fda1f23f2d054b95
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-08 14:46:38 +0700

    Update 3_3️⃣_Advanced.py

--- pages/3_3️⃣_Advanced.py
+++ pages/3_3️⃣_Advanced.py
@@ -49,7 +49,7 @@ with st.sidebar:
 penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/master/data/penguins_cleaned.csv')
 
 # Pre-process data
-## Combine user input features with entire penguins dataset; useful for encoding phase
+## Combine user input features with entire penguins data set; useful for encoding phase
 penguins = penguins_raw.drop('species', axis=1)
 input_penguins = pd.concat([input_df, penguins],axis=0)
 
