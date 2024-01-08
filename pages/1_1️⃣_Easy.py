e0bfd7c974340861d55e609b44f8b1f00ce69f86
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-08 10:22:36 +0700

    Update 1_1️⃣_Easy.py

--- pages/1_1️⃣_Easy.py
+++ pages/1_1️⃣_Easy.py
@@ -1,13 +1,16 @@
+# Import libraries
 import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
+# Set page configuration
 st.set_page_config(
     page_title='Palmer Penguins Predictor - Easy Mode',
     page_icon='🐧',
 )
 
+# Display title
 st.title('🐧 Palmer Penguins Predictor')
 st.success('Easy Mode')
 
@@ -21,6 +24,7 @@ with st.container(border=True):
     flipper_length_mm = st.slider('Flipper length (mm)', 172.0,231.0,201.0)
     body_mass_g = st.slider('Body mass (g)', 2700.0,6300.0,4207.0)
 
+# Create a DataFrame from user input
 data = {'island': island,
         'bill_length_mm': bill_length_mm,
         'bill_depth_mm': bill_depth_mm,
@@ -32,22 +36,22 @@ input_df = pd.DataFrame(data, index=[0])
 # Load data
 penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/master/data/penguins_cleaned.csv')
 
-# Data pre-processing
+# Pre-processing data
 ## Combines user input features with entire penguins dataset; useful for encoding phase
 penguins = penguins_raw.drop('species', axis=1)
 input_penguins = pd.concat([input_df, penguins],axis=0)
 
-## Encoding ordinal features
+## Encode ordinal features
 encode = ['island','gender']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 input_row = df_penguins[:1] # Selects only the first row (the user input data)
 
-## Preparing the dataframe
+## Prepare dataframe
 target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
 def target_encode(val):
     return target_mapper[val]
 
-## Separating X and y
+## Separate X and y
 X = df_penguins[1:]
 y = penguins_raw['species'].apply(target_encode)
 
@@ -59,6 +63,7 @@ clf.fit(X, y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
+# Display predicted species
 st.subheader('Predicted Species')
 penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
 st.success(str(penguins_species[prediction][0]))
