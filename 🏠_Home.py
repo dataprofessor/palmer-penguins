5452e9e6f343c8f3f1991710f5691f44f1690bd6
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-12 08:17:20 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -7,7 +7,26 @@ st.set_page_config(
 )
 
 st.title('🐧 Palmer Penguins Predictor')
-st.info('This tool allows one to predict the species of Palmer penguins as a function of their input features. It is built in 3 levels of difficulty!')
+
+with st.expander('About this app'):
+  st.markdown('**What can this app do?**')
+  st.info('This tool allows one to predict the species of Palmer penguins as a function of their input features. It is built in 3 levels of difficulty!')
+
+  st.markdown('**How to use the app?**')
+  st.warning('To use the app, 1. Select a difficulty level either from the sidebar or click on buttons under this About expander, 2. From the selected page, adjust widgets for the Input features that will trigger a prediction output of the Penguin species.')
+  # go to the sidebar and 1. Select a data set and 2. Adjust the model parameters by adjusting the various slider widgets. As a result, this would initiate the ML model building process, display the model results as well as allowing users to download the generated models and accompanying data.')
+
+  st.markdown('**Under the hood**')
+  st.markdown('Data sets:')
+  st.code('''- Palmer Penguins data set
+  ''', language='markdown')
+  
+  st.markdown('Libraries used:')
+  st.code('''- Pandas for data wrangling
+- Scikit-learn for building a machine learning model
+- Altair for chart creation
+- Streamlit for user interface
+  ''', language='markdown')
 
 st.write('👇 Choose one:')
 
